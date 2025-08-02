7b133fbc9a225fa30f63bd5cdd7f936871936552
Sara Echeverri <egsarita@gmail.com>
2025-08-01 20:22:41 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home (request):
     return render(request, 'home.html', {'name':'Sara Echeverri Gomez'})
+
+def about(request):
+    return render(request, 'about.html')
+
