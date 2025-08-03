91ee081f7581215d8fb4ff88b0b57f931dc056e3
Sara Echeverri <egsarita@gmail.com>
2025-08-02 20:16:57 -0500

    Se agrega modelo Movie y configuración de media

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='movies/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
