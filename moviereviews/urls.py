8b07dc8941804fd95a8b9acbfbe07d0ac98bcbb7
Sara Echeverri <egsarita@gmail.com>
2025-08-02 20:27:57 -0500

    Modificación en urls.py

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +25,7 @@ urlpatterns = [
     path('about/', movieViews.about, name='about'),
 ]
 
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
